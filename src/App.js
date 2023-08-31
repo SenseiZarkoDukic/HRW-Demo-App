68c607c99e63040c78bbad64d277b18c269fa3d2
SenseiZarkoDukic <zarko.dukic@gmail.com>
2023-08-31 19:22:34 +0200

    Created handleUndoLater function with setTimeout set to 2s and calling function nandleUndo after that time.

--- src/App.js
+++ src/App.js
@@ -90,6 +90,10 @@ function TabContent({ item }) {
     console.log(likes);
   }
 
+  function handleUndoLater() {
+    setTimeout(handleUndo, 2000);
+  }
+
   return (
     <div className="tab-content">
       <h4>{item.summary}</h4>
@@ -109,7 +113,7 @@ function TabContent({ item }) {
 
       <div className="tab-undo">
         <button onClick={handleUndo}>Undo</button>
-        <button>Undo in 2s</button>
+        <button onClick={handleUndoLater}>Undo in 2s</button>
       </div>
     </div>
   );
